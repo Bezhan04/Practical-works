99d83acc417da366c73efd6d5ee15254dcf91aba
Bezhan Kasymov <112756064+Bezhan04@users.noreply.github.com>
2022-12-28 12:36:31 +0300

    Add files via upload

---
+++ practiki/pr2/main/src/TestAutor.java
@@ -0,0 +1,7 @@
+public class TestAutor {
+    public static void main(String[] args) {
+        Author author1 = new Author("Pushkin", "push@hello.world", 'm');
+        author1.setEmail("stpeter@russia.ru");
+        System.out.println(author1);
+    }
+}
