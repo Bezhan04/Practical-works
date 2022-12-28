957fe2947a08031f67f6465ba9fd1ac4f85992cd
Bezhan Kasymov <112756064+Bezhan04@users.noreply.github.com>
2022-12-28 12:34:35 +0300

    Add files via upload

---
+++ labs/lab16/opt1/src/Address.java
@@ -0,0 +1,24 @@
+public class Address {
+    private String cityName; private  int  zipCode; private String streetName; private int buildingNumber; private char buildingLetter;
+    private int apartementNumber;
+
+    public String getCityName() { return cityName;
+    }
+
+    public int getZipCode() { return zipCode;
+    }
+
+    public String getStreetName() { return streetName;
+    }
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public char getBuildingLetter() { return buildingLetter;
+    }
+
+    public int getApartementNumber() { return apartementNumber;
+    }
+
+    }
