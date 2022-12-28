dac06d60d8636934e898940b36fbfd1c08ad1b10
Bezhan Kasymov <112756064+Bezhan04@users.noreply.github.com>
2022-12-28 12:28:08 +0300

    Add files via upload

---
+++ labs/lab1/src/Exe2Args.java
@@ -0,0 +1,7 @@
+public class Exe2Args {
+    public static void main(String[] args) {
+        for (String arg : args) {
+        System.out.println(arg);
+        }
+    }
+}
---
+++ labs/lab1/src/Exe3Harmonic.java
@@ -0,0 +1,7 @@
+public class Exe3Harmonic {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+        System.out.println(1. / i);
+        }
+    }
+}
